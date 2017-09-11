f2af1e8ef07cf8d29ca093311c60018df3ca7e3d
Justin Lee <justin.lee@ubc.ca>
2017-09-10 19:01:30 -0700

    working on memoized fib

---
+++ 5 kyu/memoizedFib.js
@@ -0,0 +1,13 @@
+var fibonacci = function(n, arrayOfSolutions) {
+    if (typeof array === 'undefined') {
+      let arrayOfSolutions = []
+    } 
+    if(n===0 || n === 1)
+        return n;
+    const findIfKeyExistsInArr = (arr, key) => (arr.filter(x => x.key === key).length > 0)
+    if (arrayOfSolutions.filter(x => x.key === n).length === 1) {
+      
+    }
+    
+    //return fibonacci(n-1) + fibonacci(n-2);
+}
