bc33d8ac7084c15438b744c1e8e75f09189e437e
Justin Lee <justin.lee@ubc.ca>
2017-09-10 18:38:42 -0700

    first commit

---
+++ finishSudoku.js
@@ -0,0 +1,20 @@
+function doneOrNot(board) {
+    const checkValidityOfBoard = (board) => {
+        for (let i = 0; i < 9; i++) {
+            let set = new Set()
+            for (let k = 0; k < 9; k++) {
+                set.add(board[i][k])
+            }
+            if (set.size === 9) continue
+            else return false
+        }
+        return true
+    }
+
+    const transpose = board => board[0].map((x, i) => board.map(x => x[i]))
+    const transposedBoard = transpose(board)
+
+    if (checkValidityOfBoard(board) && checkValidityOfBoard(transposedBoard)) {
+        return 'Finished!'
+    } else return 'Try again!'
+}
