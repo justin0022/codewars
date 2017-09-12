8cba2bd7cab9713528c4f16c5b822b6880c3d300
Justin Lee <justinlee0022@gmail.com>
2017-09-11 19:49:58 -0700

    started working on permutation

--- 4 kyu/permutations.js
+++ 4 kyu/permutations.js
@@ -1,4 +1,10 @@
 //In this kata you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
 function permutations(string) {
-    
+    const possibleResults = []
+    const arr = string.split('')
+    for(let i = 0; i < arr.length; i++) {
+        for(let k = 0; k < arr.length - 1; k++) {
+            
+        }
+    }
 }
