11cae5d443abd062311b995360bccd4f911bf5ff
Justin Lee <justinlee0022@gmail.com>
2017-12-13 14:37:58 -0800

    Wrote memoized implementation

--- 4 kyu/pascalTriangleRecursive.js
+++ 4 kyu/pascalTriangleRecursive.js
@@ -4,19 +4,27 @@
 // 4 => [1, 1,1, 1,2,1, 1,3,3,1]
 // 5 => [1, 1,1, 1,2,1, 1,3,3,1, 1,4,6,4,1]
 
+const memoizeArr = []
+
 const pt = (level) => {
   if (level === 1) return [1]
-  const prevArr = pt(level - 1).slice(-(level - 1))
+  let prevLevel;
+  if (memoizeArr[level - 1]) {
+    prevLevel = memoizeArr[level - 1]
+  } else {
+    prevLevel = pt(level - 1)
+  }
+  const prevArr = prevLevel.slice(-(level - 1))
   const thisArr = [1]
   for (let i = 0; i < prevArr.length - 1; i++) {
     thisArr[i + 1] = prevArr[i] + prevArr[i + 1]
   }
   thisArr.push(1)
-  return [...pt(level - 1), ...thisArr]
+  return [...prevLevel, ...thisArr]
 }
 
-console.log(pt(1))
-console.log(pt(2))
-console.log(pt(3))
-console.log(pt(4))
-console.log(pt(5))
+// console.log(pt(1))
+// console.log(pt(2))
+// console.log(pt(3))
+// console.log(pt(4))
+console.log(pt(300))
